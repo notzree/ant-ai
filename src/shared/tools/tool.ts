d239efd51d34fd35f183dc2d24d259c70f2f5b1d
notzree <richard@symphoniclabs.com>
2025-03-16 14:41:17 -0700

    fix tooling interface

--- src/shared/tools/tool.ts
+++ src/shared/tools/tool.ts
@@ -1,9 +1,19 @@
 import type { User } from "../user/user";
 
-export interface AntTool {
-  id: string; // this might be server id or somehting
+export class AntTool {
+  serverUrl: string;
   name: string;
   description: string;
-  baseServerUrl: string;
-  getUrl(user: User): Promise<string>;
+  inputSchema: object;
+  constructor(
+    serverUrl: string,
+    name: string,
+    description: string,
+    inputSchema: object,
+  ) {
+    this.serverUrl = serverUrl;
+    this.name = name;
+    this.description = description;
+    this.inputSchema = inputSchema;
+  }
 }
